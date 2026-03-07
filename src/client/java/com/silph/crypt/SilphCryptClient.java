6d430ddd507ca63710c593ddb4d5223cc8dde250
eddythedesigner <ed.mcdaniel92@gmail.com>
2026-03-07 16:33:23 -0600

    Base template

---
+++ src/client/java/com/silph/crypt/SilphCryptClient.java
@@ -0,0 +1,10 @@
+package com.silph.crypt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilphCryptClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
